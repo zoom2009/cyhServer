9e590f63060919499e45d90dd00377bd66296913
unknown <afumagic3@gmail.com>
2018-08-06 16:03:13 +0700

    api sendnotification:id/:event

--- app.js
+++ app.js
@@ -175,6 +175,8 @@ app.get('/sendnotification/:id/:event', (req, res) => {
                     function (error, response, body) {
                         if (!error && response.statusCode == 200) {
                             console.log(body)
+                        }else {
+                            console.log(response.statusCode, body)
                         }
                     }
                 );
